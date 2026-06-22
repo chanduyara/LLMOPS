3b92d2968a10dd4cd25aac18e3a15c9d99aae4e6
chanduyara <chanduyara@gmail.com>
2026-06-22 21:03:53 +0530

    Update fiesta_final_dashboard.py

--- Desktop/DSAI_IIMB/Module2/work/fiesta_final_dashboard.py
+++ Desktop/DSAI_IIMB/Module2/work/fiesta_final_dashboard.py
@@ -19,6 +19,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
+import os
 
 # ── Page config ───────────────────────────────────────────────────────────────
 st.set_page_config(
